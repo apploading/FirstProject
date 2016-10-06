0f839ba305423858805654f5a778db2b2a0703f2
Lars Bylund <lars.bylund@hotmail.com>
2016-10-07 00:42:32 +0200

    Commit 4.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Nr.3");
         System.out.println("Nr.4");
         System.out.println("Nr.5");
+        // Comments
+        System.out.println("Hallooooo!");
     }
 }
