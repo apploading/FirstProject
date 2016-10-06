065a2e70296d2198748c6af732e5221c2dc9d112
Lars Bylund <lars.bylund@hotmail.com>
2016-10-07 00:30:22 +0200

    Commit 3.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Nr.1");    }
+    public static void main(String[] args){
+        System.out.println("Nr.2");
+        System.out.println("Nr.3");
+        System.out.println("Nr.4");
+        System.out.println("Nr.5");
+    }
 }
